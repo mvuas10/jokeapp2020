f20b5b61363fab33d3eb4361ca881f673b4b31d0
Minh Vu <mvuas10@Minhs-MacBook-Air.local>
2020-12-03 13:20:54 +0100

    Banner Bar

---
+++ src/components/Toolbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Toolbar(props) {
+  return (
+    <header id="header" className="section-header scroll">
+      <a href="./">
+        <img className="icon" src={props.logo} />
+      </a>
+
+      <nav>
+        <div className="happy-title">{props.content}</div>
+      </nav>
+    </header>
+  );
+}
